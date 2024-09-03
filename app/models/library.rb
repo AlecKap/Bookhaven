f7e026d8ba2aa8d6d5f05c8d1d928f43a8c9e0f0
AlecKap <120225785+AlecKap@users.noreply.github.com>
2024-09-03 01:24:30 -0600

    feat:  Add #full_address and #num_of_books methods

--- app/models/library.rb
+++ app/models/library.rb
@@ -1,5 +1,13 @@
 class Library < ApplicationRecord
-
   has_many :library_books
   has_many :books, through: :library_books
+
+
+  def full_address
+    "#{address}, #{city}, #{state} #{zip_code}"
+  end
+
+  def number_of_books
+    books.count
+  end
 end

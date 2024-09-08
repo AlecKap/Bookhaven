4e0836156011c4f875d3c2564548b9e51d83932c
AlecKap <120225785+AlecKap@users.noreply.github.com>
2024-09-07 23:45:24 -0600

    feat: Add new and create actions to libraries resource

--- app/controllers/libraries_controller.rb
+++ app/controllers/libraries_controller.rb
@@ -6,4 +6,25 @@ class LibrariesController < ApplicationController
   def show
     @library = Library.find(params[:id])
   end
+
+  def new
+    @library = Library.new
+  end
+
+  def create
+    library = Library.new(library_params)
+
+    if library.save
+      redirect_to library
+    else
+      flash[:error] = "#{library.errors.full_messages.to_sentence}"
+      redirect_to new_library_path
+    end
+  end
+
+  private
+
+  def library_params
+    params.require(:library).permit(:name, :address, :city, :state, :zip_code)
+  end
 end

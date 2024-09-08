fa0c7ee6d4e698356f8d35d83eb500c81f480df4
AlecKap <120225785+AlecKap@users.noreply.github.com>
2024-09-08 16:32:05 -0600

    Refactor: Remove extra whitespace in GoogleBooksFacade spec

--- spec/facades/google_books_facade_spec.rb
+++ spec/facades/google_books_facade_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe GoogleBooksFacade do
        isbn = '9780061120084'
        facade = GoogleBooksFacade.new(isbn)
        book_details = facade.book_details
-       
+     
        expect(book_details).to be_a(Hash)
 
        expect(book_details).to have_key(:title)

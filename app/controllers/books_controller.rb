37a8bfc622ffe3e4a7e9c2927c103508742bf654
AlecKap <120225785+AlecKap@users.noreply.github.com>
2024-09-08 22:00:37 -0600

    feat: Add new book creation functionality

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,4 +6,22 @@ class BooksController < ApplicationController
   def show
     @book = Book.find(params[:id])
   end
+
+  def new
+    @book = Book.new
+    @library = Library.find(params[:library_id])
+  end
+
+  def create
+    facade = GoogleBooksFacade.new(params[:book][:isbn])
+    if facade.book_details.nil?
+      flash[:error] = "Book not found"
+    else
+      book = Book.new(facade.book_details)
+      book.save
+      LibraryBook.create(library_id: params[:book][:library_id], book_id: book.id)
+      redirect_to library_path(params[:book][:library_id])
+      flash[:success] = "Book added to library"
+    end
+  end
 end

f4bffb05cc44729ae0673fcf212b588241bf48cb
AlecKap <120225785+AlecKap@users.noreply.github.com>
2024-09-08 21:57:18 -0600

    Refactor: Update library book creation in seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,41 +16,6 @@ puts "..."
 
 sleep(1)
 
-library_1 = Library.create( name: Faker::Company.name,
-                            address: Faker::Address.street_address,
-                            city: Faker::Address.city,
-                            state: Faker::Address.state_abbr,
-                            zip_code: Faker::Address.zip_code,
-                            charter_number: Faker::Number.number(digits: 10))
-
-library_2 = Library.create( name: Faker::Company.name,
-                            address: Faker::Address.street_address,
-                            city: Faker::Address.city,
-                            state: Faker::Address.state_abbr,
-                            zip_code: Faker::Address.zip_code,
-                            charter_number: Faker::Number.number(digits: 10))
-
-library_3 = Library.create( name: Faker::Company.name,
-                            address: Faker::Address.street_address,
-                            city: Faker::Address.city,
-                            state: Faker::Address.state_abbr,
-                            zip_code: Faker::Address.zip_code,
-                            charter_number: Faker::Number.number(digits: 10))
-
-library_4 = Library.create( name: Faker::Company.name,
-                            address: Faker::Address.street_address,
-                            city: Faker::Address.city,
-                            state: Faker::Address.state_abbr,
-                            zip_code: Faker::Address.zip_code,
-                            charter_number: Faker::Number.number(digits: 10))
-
-library_5 = Library.create( name: Faker::Company.name,
-                            address: Faker::Address.street_address,
-                            city: Faker::Address.city,
-                            state: Faker::Address.state_abbr,
-                            zip_code: Faker::Address.zip_code,
-                            charter_number: Faker::Number.number(digits: 10))
-
 library_6 = Library.create( name: "Lulu's little library",
                             address: "3605 W Berry Ave",
                             city: "Littleton",
@@ -122,16 +87,9 @@ puts "22 Books Created"
 
 # Library Books
 
-LibraryBook.create(library_id: library_1.id, book_id: book_1.id, quality: Faker::Number.between(from: 1, to: 5))
-LibraryBook.create(library_id: library_1.id, book_id: book_2.id, quality: Faker::Number.between(from: 1, to: 5))
-LibraryBook.create(library_id: library_1.id, book_id: book_3.id, quality: Faker::Number.between(from: 1, to: 5))
-LibraryBook.create(library_id: library_1.id, book_id: book_4.id, quality: Faker::Number.between(from: 1, to: 5))
-LibraryBook.create(library_id: library_2.id, book_id: book_5.id, quality: Faker::Number.between(from: 1, to: 5))
-LibraryBook.create(library_id: library_2.id, book_id: book_6.id, quality: Faker::Number.between(from: 1, to: 5))
-LibraryBook.create(library_id: library_2.id, book_id: book_7.id, quality: Faker::Number.between(from: 1, to: 5))
-LibraryBook.create(library_id: library_3.id, book_id: book_8.id, quality: Faker::Number.between(from: 1, to: 5))
-LibraryBook.create(library_id: library_4.id, book_id: book_9.id, quality: Faker::Number.between(from: 1, to: 5))
-LibraryBook.create(library_id: library_5.id, book_id: book_10.id, quality: Faker::Number.between(from: 1, to: 5))
+LibraryBook.create(library_id: library_11.id, book_id: book_1.id, quality: Faker::Number.between(from: 1, to: 5))
+LibraryBook.create(library_id: library_11.id, book_id: book_2.id, quality: Faker::Number.between(from: 1, to: 5))
+LibraryBook.create(library_id: library_11.id, book_id: book_9.id, quality: Faker::Number.between(from: 1, to: 5))
 LibraryBook.create(library_id: library_6.id, book_id: book_11.id, quality: Faker::Number.between(from: 1, to: 5))
 LibraryBook.create(library_id: library_6.id, book_id: book_12.id, quality: Faker::Number.between(from: 1, to: 5))
 LibraryBook.create(library_id: library_6.id, book_id: book_13.id, quality: Faker::Number.between(from: 1, to: 5))
@@ -140,7 +98,7 @@ LibraryBook.create(library_id: library_7.id, book_id: book_15.id, quality: Faker
 LibraryBook.create(library_id: library_7.id, book_id: book_16.id, quality: Faker::Number.between(from: 1, to: 5))
 LibraryBook.create(library_id: library_8.id, book_id: book_17.id, quality: Faker::Number.between(from: 1, to: 5))
 LibraryBook.create(library_id: library_9.id, book_id: book_18.id, quality: Faker::Number.between(from: 1, to: 5))
-LibraryBook.create(library_id: library_9.id, book_id: book_19.id, quality: Faker::Number.between(from: 1, to: 5))
+LibraryBook.create(library_id: library_11.id, book_id: book_19.id, quality: Faker::Number.between(from: 1, to: 5))
 LibraryBook.create(library_id: library_10.id, book_id: book_20.id, quality: Faker::Number.between(from: 1, to: 5))
 LibraryBook.create(library_id: library_11.id, book_id: book_21.id, quality: Faker::Number.between(from: 1, to: 5))
 LibraryBook.create(library_id: library_11.id, book_id: book_22.id, quality: Faker::Number.between(from: 1, to: 5))

333d958b499693184db104ee0e34cfa946e44ebd
Thomas Hawley <thawley22@turing.edu>
2024-09-11 10:03:28 -0600

    Refactor seeds.rb to include a test user and use FactoryBot for creating libraries, and books

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,6 +10,16 @@ puts "Seeding Data"
 
 sleep(1)
 
+library_1 = FactoryBot.create(:library)
+
+library_2 = FactoryBot.create(:library)
+
+library_3 = FactoryBot.create(:library)
+
+library_4 = FactoryBot.create(:library)
+
+library_5 = FactoryBot.create(:library)
+
 library_6 = Library.create( name: "Lulu's little library",
                             address: "3605 W Berry Ave",
                             city: "Littleton",
@@ -50,44 +60,51 @@ library_11 = Library.create(name: "Test Library",
                             zip_code: Faker::Address.zip_code,
                             charter_number: Faker::Number.number(digits: 10))
 
-puts "11 Libraries Created"
+puts "#{Library.count} Libraries Created"
 
 sleep(1)
 puts "."
 # Books
 
-book_1 = Book.create(title: Faker::Book.title, author: Faker::Book.author, genre: Faker::Book.genre, initial_rating: Faker::Number.between(from: 1, to: 5), description: Faker::Lorem.paragraph(sentence_count: 3), isbn_10: Faker::Code.isbn, isbn_13: Faker::Code.isbn, thumbnail_img: Faker::Avatar.image, sm_thumbnail_img: Faker::Avatar.image)
-book_2 = Book.create(title: Faker::Book.title, author: Faker::Book.author, genre: Faker::Book.genre, initial_rating: Faker::Number.between(from: 1, to: 5), description: Faker::Lorem.paragraph(sentence_count: 3), isbn_10: Faker::Code.isbn, isbn_13: Faker::Code.isbn, thumbnail_img: Faker::Avatar.image, sm_thumbnail_img: Faker::Avatar.image)
-book_3 = Book.create(title: Faker::Book.title, author: Faker::Book.author, genre: Faker::Book.genre, initial_rating: Faker::Number.between(from: 1, to: 5), description: Faker::Lorem.paragraph(sentence_count: 3), isbn_10: Faker::Code.isbn, isbn_13: Faker::Code.isbn, thumbnail_img: Faker::Avatar.image, sm_thumbnail_img: Faker::Avatar.image)
-book_4 = Book.create(title: Faker::Book.title, author: Faker::Book.author, genre: Faker::Book.genre, initial_rating: Faker::Number.between(from: 1, to: 5), description: Faker::Lorem.paragraph(sentence_count: 3), isbn_10: Faker::Code.isbn, isbn_13: Faker::Code.isbn, thumbnail_img: Faker::Avatar.image, sm_thumbnail_img: Faker::Avatar.image)
-book_5 = Book.create(title: Faker::Book.title, author: Faker::Book.author, genre: Faker::Book.genre, initial_rating: Faker::Number.between(from: 1, to: 5), description: Faker::Lorem.paragraph(sentence_count: 3), isbn_10: Faker::Code.isbn, isbn_13: Faker::Code.isbn, thumbnail_img: Faker::Avatar.image, sm_thumbnail_img: Faker::Avatar.image)
-book_6 = Book.create(title: Faker::Book.title, author: Faker::Book.author, genre: Faker::Book.genre, initial_rating: Faker::Number.between(from: 1, to: 5), description: Faker::Lorem.paragraph(sentence_count: 3), isbn_10: Faker::Code.isbn, isbn_13: Faker::Code.isbn, thumbnail_img: Faker::Avatar.image, sm_thumbnail_img: Faker::Avatar.image)
-book_7 = Book.create(title: Faker::Book.title, author: Faker::Book.author, genre: Faker::Book.genre, initial_rating: Faker::Number.between(from: 1, to: 5), description: Faker::Lorem.paragraph(sentence_count: 3), isbn_10: Faker::Code.isbn, isbn_13: Faker::Code.isbn, thumbnail_img: Faker::Avatar.image, sm_thumbnail_img: Faker::Avatar.image)
-book_8 = Book.create(title: Faker::Book.title, author: Faker::Book.author, genre: Faker::Book.genre, initial_rating: Faker::Number.between(from: 1, to: 5), description: Faker::Lorem.paragraph(sentence_count: 3), isbn_10: Faker::Code.isbn, isbn_13: Faker::Code.isbn, thumbnail_img: Faker::Avatar.image, sm_thumbnail_img: Faker::Avatar.image)
-book_9 = Book.create(title: Faker::Book.title, author: Faker::Book.author, genre: Faker::Book.genre, initial_rating: Faker::Number.between(from: 1, to: 5), description: Faker::Lorem.paragraph(sentence_count: 3), isbn_10: Faker::Code.isbn, isbn_13: Faker::Code.isbn, thumbnail_img: Faker::Avatar.image, sm_thumbnail_img: Faker::Avatar.image)
-book_10 = Book.create(title: Faker::Book.title, author: Faker::Book.author, genre: Faker::Book.genre, initial_rating: Faker::Number.between(from: 1, to: 5), description: Faker::Lorem.paragraph(sentence_count: 3), isbn_10: Faker::Code.isbn, isbn_13: Faker::Code.isbn, thumbnail_img: Faker::Avatar.image, sm_thumbnail_img: Faker::Avatar.image)
-book_11 = Book.create(title: Faker::Book.title, author: Faker::Book.author, genre: Faker::Book.genre, initial_rating: Faker::Number.between(from: 1, to: 5), description: Faker::Lorem.paragraph(sentence_count: 3), isbn_10: Faker::Code.isbn, isbn_13: Faker::Code.isbn, thumbnail_img: Faker::Avatar.image, sm_thumbnail_img: Faker::Avatar.image)
-book_12 = Book.create(title: Faker::Book.title, author: Faker::Book.author, genre: Faker::Book.genre, initial_rating: Faker::Number.between(from: 1, to: 5), description: Faker::Lorem.paragraph(sentence_count: 3), isbn_10: Faker::Code.isbn, isbn_13: Faker::Code.isbn, thumbnail_img: Faker::Avatar.image, sm_thumbnail_img: Faker::Avatar.image)
-book_13 = Book.create(title: Faker::Book.title, author: Faker::Book.author, genre: Faker::Book.genre, initial_rating: Faker::Number.between(from: 1, to: 5), description: Faker::Lorem.paragraph(sentence_count: 3), isbn_10: Faker::Code.isbn, isbn_13: Faker::Code.isbn, thumbnail_img: Faker::Avatar.image, sm_thumbnail_img: Faker::Avatar.image)
-book_14 = Book.create(title: Faker::Book.title, author: Faker::Book.author, genre: Faker::Book.genre, initial_rating: Faker::Number.between(from: 1, to: 5), description: Faker::Lorem.paragraph(sentence_count: 3), isbn_10: Faker::Code.isbn, isbn_13: Faker::Code.isbn, thumbnail_img: Faker::Avatar.image, sm_thumbnail_img: Faker::Avatar.image)
-book_15 = Book.create(title: Faker::Book.title, author: Faker::Book.author, genre: Faker::Book.genre, initial_rating: Faker::Number.between(from: 1, to: 5), description: Faker::Lorem.paragraph(sentence_count: 3), isbn_10: Faker::Code.isbn, isbn_13: Faker::Code.isbn, thumbnail_img: Faker::Avatar.image, sm_thumbnail_img: Faker::Avatar.image)
-book_16 = Book.create(title: Faker::Book.title, author: Faker::Book.author, genre: Faker::Book.genre, initial_rating: Faker::Number.between(from: 1, to: 5), description: Faker::Lorem.paragraph(sentence_count: 3), isbn_10: Faker::Code.isbn, isbn_13: Faker::Code.isbn, thumbnail_img: Faker::Avatar.image, sm_thumbnail_img: Faker::Avatar.image)
-book_17 = Book.create(title: Faker::Book.title, author: Faker::Book.author, genre: Faker::Book.genre, initial_rating: Faker::Number.between(from: 1, to: 5), description: Faker::Lorem.paragraph(sentence_count: 3), isbn_10: Faker::Code.isbn, isbn_13: Faker::Code.isbn, thumbnail_img: Faker::Avatar.image, sm_thumbnail_img: Faker::Avatar.image)
-book_18 = Book.create(title: Faker::Book.title, author: Faker::Book.author, genre: Faker::Book.genre, initial_rating: Faker::Number.between(from: 1, to: 5), description: Faker::Lorem.paragraph(sentence_count: 3), isbn_10: Faker::Code.isbn, isbn_13: Faker::Code.isbn, thumbnail_img: Faker::Avatar.image, sm_thumbnail_img: Faker::Avatar.image)
-book_19 = Book.create(title: Faker::Book.title, author: Faker::Book.author, genre: Faker::Book.genre, initial_rating: Faker::Number.between(from: 1, to: 5), description: Faker::Lorem.paragraph(sentence_count: 3), isbn_10: Faker::Code.isbn, isbn_13: Faker::Code.isbn, thumbnail_img: Faker::Avatar.image, sm_thumbnail_img: Faker::Avatar.image)
-book_20 = Book.create(title: Faker::Book.title, author: Faker::Book.author, genre: Faker::Book.genre, initial_rating: Faker::Number.between(from: 1, to: 5), description: Faker::Lorem.paragraph(sentence_count: 3), isbn_10: Faker::Code.isbn, isbn_13: Faker::Code.isbn, thumbnail_img: Faker::Avatar.image, sm_thumbnail_img: Faker::Avatar.image)
-book_21 = Book.create(title: Faker::Book.title, author: Faker::Book.author, genre: Faker::Book.genre, initial_rating: Faker::Number.between(from: 1, to: 5), description: Faker::Lorem.paragraph(sentence_count: 3), isbn_10: Faker::Code.isbn, isbn_13: Faker::Code.isbn, thumbnail_img: Faker::Avatar.image, sm_thumbnail_img: Faker::Avatar.image)
-book_22 = Book.create(title: Faker::Book.title, author: Faker::Book.author, genre: Faker::Book.genre, initial_rating: Faker::Number.between(from: 1, to: 5), description: Faker::Lorem.paragraph(sentence_count: 3), isbn_10: Faker::Code.isbn, isbn_13: Faker::Code.isbn, thumbnail_img: Faker::Avatar.image, sm_thumbnail_img: Faker::Avatar.image)
+book_1 = FactoryBot.create(:book)
+book_2 = FactoryBot.create(:book)
+book_3 = FactoryBot.create(:book)
+book_4 = FactoryBot.create(:book)
+book_5 = FactoryBot.create(:book)
+book_6 = FactoryBot.create(:book)
+book_7 = FactoryBot.create(:book)
+book_8 = FactoryBot.create(:book)
+book_9 = FactoryBot.create(:book)
+book_10 = FactoryBot.create(:book)
+book_11 = FactoryBot.create(:book)
+book_12 = FactoryBot.create(:book)
+book_13 = FactoryBot.create(:book)
+book_14 = FactoryBot.create(:book)
+book_15 = FactoryBot.create(:book)
+book_16 = FactoryBot.create(:book)
+book_17 = FactoryBot.create(:book)
+book_18 = FactoryBot.create(:book)
+book_19 = FactoryBot.create(:book)
+book_20 = FactoryBot.create(:book)
+book_21 = FactoryBot.create(:book)
+book_22 = FactoryBot.create(:book)
 
 sleep(1)
 puts ".."
-puts "22 Books Created"
+puts "#{Book.count} Books Created"
 
 # Library Books
 
-LibraryBook.create(library_id: library_11.id, book_id: book_1.id, quality: Faker::Number.between(from: 1, to: 5))
-LibraryBook.create(library_id: library_11.id, book_id: book_2.id, quality: Faker::Number.between(from: 1, to: 5))
-LibraryBook.create(library_id: library_11.id, book_id: book_9.id, quality: Faker::Number.between(from: 1, to: 5))
+LibraryBook.create(library_id: library_1.id, book_id: book_1.id, quality: Faker::Number.between(from: 1, to: 5))
+LibraryBook.create(library_id: library_1.id, book_id: book_2.id, quality: Faker::Number.between(from: 1, to: 5))
+LibraryBook.create(library_id: library_1.id, book_id: book_3.id, quality: Faker::Number.between(from: 1, to: 5))
+LibraryBook.create(library_id: library_1.id, book_id: book_4.id, quality: Faker::Number.between(from: 1, to: 5))
+LibraryBook.create(library_id: library_2.id, book_id: book_5.id, quality: Faker::Number.between(from: 1, to: 5))
+LibraryBook.create(library_id: library_2.id, book_id: book_6.id, quality: Faker::Number.between(from: 1, to: 5))
+LibraryBook.create(library_id: library_2.id, book_id: book_7.id, quality: Faker::Number.between(from: 1, to: 5))
+LibraryBook.create(library_id: library_3.id, book_id: book_8.id, quality: Faker::Number.between(from: 1, to: 5))
+LibraryBook.create(library_id: library_4.id, book_id: book_9.id, quality: Faker::Number.between(from: 1, to: 5))
+LibraryBook.create(library_id: library_5.id, book_id: book_10.id, quality: Faker::Number.between(from: 1, to: 5))
 LibraryBook.create(library_id: library_6.id, book_id: book_11.id, quality: Faker::Number.between(from: 1, to: 5))
 LibraryBook.create(library_id: library_6.id, book_id: book_12.id, quality: Faker::Number.between(from: 1, to: 5))
 LibraryBook.create(library_id: library_6.id, book_id: book_13.id, quality: Faker::Number.between(from: 1, to: 5))
@@ -96,7 +113,7 @@ LibraryBook.create(library_id: library_7.id, book_id: book_15.id, quality: Faker
 LibraryBook.create(library_id: library_7.id, book_id: book_16.id, quality: Faker::Number.between(from: 1, to: 5))
 LibraryBook.create(library_id: library_8.id, book_id: book_17.id, quality: Faker::Number.between(from: 1, to: 5))
 LibraryBook.create(library_id: library_9.id, book_id: book_18.id, quality: Faker::Number.between(from: 1, to: 5))
-LibraryBook.create(library_id: library_11.id, book_id: book_19.id, quality: Faker::Number.between(from: 1, to: 5))
+LibraryBook.create(library_id: library_9.id, book_id: book_19.id, quality: Faker::Number.between(from: 1, to: 5))
 LibraryBook.create(library_id: library_10.id, book_id: book_20.id, quality: Faker::Number.between(from: 1, to: 5))
 LibraryBook.create(library_id: library_11.id, book_id: book_21.id, quality: Faker::Number.between(from: 1, to: 5))
 LibraryBook.create(library_id: library_11.id, book_id: book_22.id, quality: Faker::Number.between(from: 1, to: 5))
@@ -104,4 +121,8 @@ LibraryBook.create(library_id: library_11.id, book_id: book_22.id, quality: Fake
 sleep(1)
 puts "..."
 
-puts "22 LibraryBooks Created"
+puts "#{LibraryBook.count} LibraryBooks Created"
+
+User.create(email: "test@example.com", password: "password", first_name: "Test", last_name: "User")
+
+puts "User Created, test@example.com, password"
